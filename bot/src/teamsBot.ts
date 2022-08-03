99c88430859342175b999006532c2c84da8f734e
ahatli <moonks830@gmail.com>
2022-08-03 10:18:43 +0900

    Action.Execute -> Action.Submit 수정

--- bot/src/teamsBot.ts
+++ bot/src/teamsBot.ts
@@ -31,31 +31,54 @@ export class TeamsBot extends TeamsActivityHandler {
       message.type = ActivityTypes.Typing;
       await context.sendActivity(message);
 
-      let txt = context.activity.text;
-      const removedMentionText = TurnContext.removeRecipientMention(context.activity);
-      if (removedMentionText) {
-        txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
-      }
-
-      const text = txt.split(" ");
-      if (text[0] === '근무지등록') {
-        setWorkplaceForm(context.activity.from.id, text[1], 'work');
-      } else if (text[0] + text[1] === '근무지등록') {
-        setWorkplaceForm(context.activity.from.id, text[2], 'work');
-      } else if (text[0] === '근무지') {
-        getWorkplace(context.activity.from.id, text[1], text[2]);
-      } else if (text[0] === '홈' || text[0].toLowerCase() === 'home' || text[0] === 'ㅎ') {
-        sendCommand(context.activity.from.id);
-      } else if (text[0] === '메시지' || text[0] === '메세지') {
-        viewSecretMessage(context.activity.from.id, text[1]);
-      } else if (text[0] === 'birthTest') {
-        sendBirthdayCard();
-      } else if (text[0] === 'workplaceTestSend') {
-        setWorkplaceForm(null, null, 'send');
-      } else if (text[0] === 'workplaceTestResend') {
-        setWorkplaceForm(null, null, 'resend');
-      } else {
-        sorryMessage(context.activity.from.id);
+      if(context.activity.text) {
+        let txt = context.activity.text;
+        const removedMentionText = TurnContext.removeRecipientMention(context.activity);
+        if (removedMentionText) {
+          txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
+        }
+        const text = txt.split(" ");
+        if (text[0] === '근무지등록') {
+          setWorkplaceForm(context.activity.from.id, text[1], 'work');
+        } else if (text[0] + text[1] === '근무지등록') {
+          setWorkplaceForm(context.activity.from.id, text[2], 'work');
+        } else if (text[0] === '근무지') {
+          getWorkplace(context.activity.from.id, text[1], text[2]);
+        } else if (text[0] === '홈' || text[0].toLowerCase() === 'home' || text[0] === 'ㅎ') {
+          sendCommand(context.activity.from.id);
+        } else if (text[0] === '메시지' || text[0] === '메세지') {
+          viewSecretMessage(context.activity.from.id, text[1]);
+        } else if (text[0] === 'birthTest') {
+          sendBirthdayCard();
+        } else if (text[0] === 'workplaceTestSend') {
+          setWorkplaceForm(null, null, 'send');
+        } else if (text[0] === 'workplaceTestResend') {
+          setWorkplaceForm(null, null, 'resend');
+        } else {
+          sorryMessage(context.activity.from.id);
+        }
+      } else if(context.activity.value) {
+        if (context.activity.value.messageType === "getWorkplaceForm") {
+          getWorkplaceForm(context.activity.from.id);
+        } else if (context.activity.value.messageType === "getWorkplace") {  
+          getWorkplace(context.activity.from.id, context.activity.value.username, null);
+        } else if (context.activity.value.messageType === "setWorkplace") {  
+          setWorkplace(context.activity.from.id, context.activity.value.UPN, context.activity.value.WorkDate, context.activity.value.WorkCodeAM, context.activity.value.WorkCodePM);
+        } else if (context.activity.value.messageType === "setWorkplaceForm") {
+          setWorkplaceForm(context.activity.from.id, null, 'work');
+        } else if (context.activity.value.messageType === "viewSecretMessage") {
+          viewSecretMessage(context.activity.from.id, null);
+        } else if (context.activity.value.messageType === "sendSecretMessage") {  
+          sendSecretMessage(context.activity.from.id, context.activity.value.receiver, context.activity.value.senderNick, context.activity.value.message);
+        } else if (context.activity.value.messageType === "openSecretMessage") {  
+          openSecretMessage(context.activity.from.id, context.activity.value.messageId, context);
+        } else if (context.activity.value.messageType === "openBirthMessage") {  
+          openBirthMessage(context.activity.from.id, context.activity.value.messageId, context.activity.value.username, context.activity.value.birthDate);
+        } else if (context.activity.value.messageType === "viewCommandList") {  
+          viewCommandList(context.activity.from.id);
+        } else {
+          sorryMessage(context.activity.from.id);
+        }
       }
 
       await next();
@@ -82,40 +105,5 @@ export class TeamsBot extends TeamsActivityHandler {
       }
       await next();
     });
-  }
-
-  async onAdaptiveCardInvoke(
-    context: TurnContext,
-    invokeValue: AdaptiveCardInvokeValue
-  ): Promise<AdaptiveCardInvokeResponse> {
-    
-    console.log('invokeValue ' + JSON.stringify(invokeValue));
-
-    const message = MessageFactory.text('');
-    message.type = ActivityTypes.Typing;
-    await context.sendActivity(message);
-
-    if (invokeValue.action.verb === "getWorkplaceForm") {
-      getWorkplaceForm(context.activity.from.id);
-    } else if (invokeValue.action.verb === "getWorkplace") {  
-      getWorkplace(context.activity.from.id, invokeValue.action.data.username, null);
-    } else if (invokeValue.action.verb === "setWorkplace") {  
-      setWorkplace(context.activity.from.id, invokeValue.action.data.UPN, invokeValue.action.data.WorkDate, invokeValue.action.data.WorkCodeAM, invokeValue.action.data.WorkCodePM);
-    } else if (invokeValue.action.verb === "setWorkplaceForm") {
-      setWorkplaceForm(context.activity.from.id, null, 'work');
-    } else if (invokeValue.action.verb === "viewSecretMessage") {
-      viewSecretMessage(context.activity.from.id, null);
-    } else if (invokeValue.action.verb === "sendSecretMessage") {  
-      sendSecretMessage(context.activity.from.id, invokeValue.action.data.receiver, invokeValue.action.data.senderNick, invokeValue.action.data.message);
-    } else if (invokeValue.action.verb === "openSecretMessage") {  
-      openSecretMessage(context.activity.from.id, invokeValue.action.data.messageId);
-    } else if (invokeValue.action.verb === "openBirthMessage") {  
-      openBirthMessage(context.activity.from.id, invokeValue.action.data.messageId, invokeValue.action.data.username, invokeValue.action.data.birthDate);
-    } else if (invokeValue.action.verb === "viewCommandList") {  
-      viewCommandList(context.activity.from.id);
-    } else {
-      sorryMessage(context.activity.from.id);
-    }
-    return { statusCode: 200, type: undefined, value: undefined };
   }
 }
