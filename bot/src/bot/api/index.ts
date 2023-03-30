841766140578596734536f0f9b7b9262dd0b6c7c
ahat <moonks830@gmail.com>
2023-03-31 01:20:15 +0900

    코드 정리

--- bot/src/bot/api/index.ts
+++ bot/src/bot/api/index.ts
@@ -18,162 +18,164 @@ import {
 
 export const routerInstance = new Router();
 
-routerInstance.get('/getWorkplace', async (req, res) => {
-  if (!await ValidationToken(req.authorization.credentials, req.getUrl().path)) {
-    await insertLog('', '인증실패');
-  }
+const ValidationTokenFunc = async (req, res, func) => {
+  try {
+    if (!await ValidationToken(req.authorization.credentials, req.getUrl().path)) {
+      await insertLog('', '인증실패');
+    }
+    
+    func();
 
-  const row = await UspGetWorkplaceTeam(req.query["startDate"], req.query["endDate"], req.query["team"]);
-  res.json(row);
+  } catch (e) {
+    await insertLog('', "Error : " + JSON.stringify(e) + ", " + e?.message);
+    res.json({message: "Invalid request"});
+  }
+}
+
+const ValidationGatewayFunc = async (req, res, func) => {
+  try {
+    if (!await ValidationTokenGateway(req.authorization.credentials)) {
+      await insertLog('', '인증실패');
+    }
+    
+    func();
+
+  } catch (e) {
+    await insertLog('', "Error : " + JSON.stringify(e) + ", " + e?.message);
+    res.json({message: "Invalid request"});
+  }
+}
+
+routerInstance.get('/getWorkplace', async (req, res) => {
+  await ValidationTokenFunc(req, res, async () => {
+    const row = await UspGetWorkplaceTeam(req.query["startDate"], req.query["endDate"], req.query["team"]);
+    res.json(row);
+  });
 });
 
 routerInstance.get('/getTeam', async (req, res) => {
-  if (!await ValidationToken(req.authorization.credentials, req.getUrl().path)) {
-    await insertLog('', '인증실패');
-  }
-
-  const row = await UspGetTeam(req.query["UPN"]);
-  res.json(row);
+  await ValidationTokenFunc(req, res, async () => {
+    const row = await UspGetTeam(req.query["UPN"]);
+    res.json(row);
+  });
 });
 
 routerInstance.get('/getStore', async (req, res) => {
-  if (!await ValidationToken(req.authorization.credentials, req.getUrl().path)) {
-    await insertLog('', '인증실패');
-  }
-
-  const row = await UspGetStore(req.query["search"], req.query["category"]);
-  res.json(row);
+  await ValidationTokenFunc(req, res, async () => {
+    const row = await UspGetStore(req.query["search"], req.query["category"]);
+    res.json(row);
+  });
 });
 
 routerInstance.get('/tag', async (req, res) => {
-  if (!await ValidationToken(req.authorization.credentials, req.getUrl().path)) {
-    await insertLog('', '인증실패');
-  }
-
-  const row = await UspGetTag(Number(req.query["storeId"]));
-  res.json(row);
+  await ValidationTokenFunc(req, res, async () => {
+    const row = await UspGetTag(Number(req.query["storeId"]));
+    res.json(row);
+  });
 });
 
 routerInstance.post('/tag', async (req, res) => {
-  if (!await ValidationToken(req.authorization.credentials, req.getUrl().path)) {
-    await insertLog('', '인증실패');
-  }
-  
-  const row = await UspSetTag(Number(req.body["storeId"]), req.body["tag"], req.body["UPN"]);
-  res.json(row);
+  await ValidationTokenFunc(req, res, async () => {
+    const row = await UspSetTag(Number(req.body["storeId"]), req.body["tag"], req.body["UPN"]);
+    res.json(row);
+  });
 });
 
 routerInstance.del('/tag', async (req, res) => {
-  if (!await ValidationToken(req.authorization.credentials, req.getUrl().path)) {
-    await insertLog('', '인증실패');
-  }
-  
-  const row = await UspDeleteTag(Number(req.query["storeId"]), req.query["tag"], req.query["UPN"]);
-  res.json(row);
-});
-
-routerInstance.post("/sendUserMessage", async (req, res) => {  
-  if (!await ValidationToken(req.authorization.credentials, req.getUrl().path)) {
-    await insertLog('', '인증실패');
-  }
-  
-  const row = await SendUserMessage(req.body.id, req.body.message);
-  res.json(row);
+  await ValidationTokenFunc(req, res, async () => {
+    const row = await UspDeleteTag(Number(req.query["storeId"]), req.query["tag"], req.query["UPN"]);
+    res.json(row);
+  });
 });
 
 routerInstance.post("/setWorkplace", async (req, res) => {  
-  if (!await ValidationToken(req.authorization.credentials, req.getUrl().path)) {
-    await insertLog('', '인증실패');
-  }
-  
-  if(!req.body.workDate || !req.body.upn) {
-    return;
-  }
-  const row = await UspSetWorkplace(req.body.workDate, req.body.upn, req.body.workCodeAM===''?null:req.body.workCodeAM, req.body.workCodePM===''?null:req.body.workCodePM);
-  res.json(row);
+  await ValidationTokenFunc(req, res, async () => {
+    if(!req.body.workDate || !req.body.upn) {
+      return;
+    }
+    const row = await UspSetWorkplace(req.body.workDate, req.body.upn, req.body.workCodeAM===''?null:req.body.workCodeAM, req.body.workCodePM===''?null:req.body.workCodePM);
+    res.json(row);
+  });
 });
 
 routerInstance.get('/getWorkCode', async (req, res) => {
-  if (!await ValidationToken(req.authorization.credentials, req.getUrl().path)) {
-    await insertLog('', '인증실패');
-  }
-  
-  const row = await UspGetWorkCode();
-  res.json(row);
+  await ValidationTokenFunc(req, res, async () => {
+    const row = await UspGetWorkCode();
+    res.json(row);
+  });
 });
 
 // Message
 
 routerInstance.get("/getGroupChat", async (req, res) => {  
-  if (!await ValidationTokenGateway(req.authorization.credentials)) {
-    await insertLog('', '인증실패');
-  }
+  await ValidationGatewayFunc(req, res, async () => {
+    const arr = [];
+    for(const data of Object.entries(groupChatMap)) {
+      arr.push({
+        type: data[1]?.conversationReference?.conversation?.conversationType,
+        name: data[1]?.conversationReference?.conversation?.name?data[1]?.conversationReference?.conversation?.name:'일반',
+        id: data[1]?.conversationReference?.conversation?.id,
+        teamName: data[1]?.TeamName
+      });
+      console.log(JSON.stringify(data));
+    }
+  
+    res.json(arr);
+  });
+});
 
-  const arr = [];
-  for(const data of Object.entries(groupChatMap)) {
-    arr.push({
-      type: data[1]?.conversationReference?.conversation?.conversationType,
-      name: data[1]?.conversationReference?.conversation?.name?data[1]?.conversationReference?.conversation?.name:'일반',
-      id: data[1]?.conversationReference?.conversation?.id,
-      teamName: data[1]?.TeamName
-    });
-    console.log(JSON.stringify(data));
-  }
-
-  res.json(arr);
+routerInstance.post("/sendUserMessage", async (req, res) => {
+  await ValidationGatewayFunc(req, res, async () => {
+    const row = await SendUserMessage(req.body.user, req.body.message);
+    res.json(row);
+  });
 });
 
 routerInstance.post("/sendGroupMessage", async (req, res) => {  
-  if (!await ValidationTokenGateway(req.authorization.credentials)) {
-    await insertLog('', '인증실패');
-  }
-
-  const row = await SendGroupMessage(req.body.id, req.body.message);
-  res.json(row);
+  await ValidationGatewayFunc(req, res, async () => {
+    const row = await SendGroupMessage(req.body.id, req.body.message);
+    res.json(row);
+  });
 });
 
 routerInstance.post("/sendGroupMentionMessage", async (req, res) => {  
-  if (!await ValidationTokenGateway(req.authorization.credentials)) {
-    await insertLog('', '인증실패');
-  }
-
-  const groupChat = <TeamsBotInstallation>groupChatMap[req.body.id];
-  if(!groupChat) {
-    res.json({message: "Invalid chat Id"});
-  }
-
-  const row = await SendMentionMessage(groupChat, req.body.user, req.body.message);
-  res.json(row);
+  await ValidationGatewayFunc(req, res, async () => {
+    const groupChat = <TeamsBotInstallation>groupChatMap[req.body.id];
+    if(!groupChat) {
+      res.json({message: "Invalid chat Id"});
+      return;
+    }
+  
+    const row = await SendMentionMessage(groupChat, req.body.user, req.body.message);
+    res.json(row);
+  });
 });
 
+const SendUserMessage = async (userInfo: string, message: string) => {
+  if(!userInfo || !message) {
+    return {message: "Invalid request"};
+  }
+  
+  let user = <Member>null;
+  for (const u of Object.entries(userMap)) {
+    if(<string>(u[1].account.userPrincipalName).toLowerCase() === userInfo.toLowerCase()) {
+      user = <Member>u[1];
+      break;
+    }
+    
+    if(u[1].FullNameKR === userInfo) {
+      user = <Member>u[1];
+      break;
+    }
+  }
 
-
-
-const SendUserMessage = async (id: string, message: string) => {
-  if(!id || !message) {
+  if(user === null) {
     return {message: "Invalid request"};
   }
 
-  const user = <Member>userMap[id];
-  console.log(JSON.stringify(groupChatMap));
-  if(!user) {
-    return {message: "Invalid chat Id"};
-  }
+  const messageActivity = MakeMessage(message);
 
-  
-  const mention: Mention = {
-    mentioned: user.account,
-    text: `<at> </at>`,
-    type: 'mention'
-  };
-
-  const message2: Partial<Activity> = {
-      entities: [mention],
-      text: message.replace('문광석', mention.text),
-      type: ActivityTypes.Message
-  };
-
-  return await user.sendMessage(<string>message2);
+  return await user.sendMessage(<string>messageActivity);
 }
 
 const SendGroupMessage = async (id: string, message: string) => {
@@ -186,55 +188,8 @@ const SendGroupMessage = async (id: string, message: string) => {
     if(!groupChat) {
       return {message: "Invalid chat Id"};
     }
-  
-    const mentionArr = [];
-    let text = message;
-    for(let i = 0;;i++) {
-      const start = text.indexOf('<mention>');
-      const end = text.indexOf('</mention>');
-  
-      if(start === -1 || end === -1) {
-        break;
-      }
-  
-      const userInfo = text.substring(start+9, end);
-  
-      let user = <Member>null;
-      for (const u of Object.entries(userMap)) {
-        if(<string>(u[1].account.userPrincipalName).toLowerCase() === userInfo.toLowerCase()) {
-          user = <Member>u[1];
-          break;
-        }
-        
-        if(u[1].FullNameKR === userInfo) {
-          user = <Member>u[1];
-          break;
-        }
-      }
-  
-      if(user === null) {
-        text = text.substring(end+10);
-        continue;
-      }
-      
-      const mText = text.substring(start, end+10);
-      const rText = `<at> ${i} </at>`;
-      message = message.replace(mText, rText);
-  
-      const mention: Mention = {
-          mentioned: user.account,
-          text: rText,
-          type: 'mention'
-      };
-      mentionArr.push(mention);
-      text = text.substring(end+10);
-    }
-  
-    const messageActivity: Partial<Activity> = {
-      entities: mentionArr,
-      text: message,
-      type: ActivityTypes.Message
-    };
+
+    const messageActivity = MakeMessage(message);
   
     return await groupChat.sendMessage(<string>messageActivity);
   } catch(e) {
@@ -242,6 +197,61 @@ const SendGroupMessage = async (id: string, message: string) => {
   }
 }
 
+const MakeMessage = (message: string):Partial<Activity>  => {  
+  const mentionArr = [];
+  let text = message;
+  for(let i = 0;;i++) {
+    const start = text.indexOf('<mention>');
+    const end = text.indexOf('</mention>');
+
+    if(start === -1 || end === -1) {
+      break;
+    }
+
+    const userInfo = text.substring(start+9, end);
+
+    let user = <Member>null;
+    for (const u of Object.entries(userMap)) {
+      if(<string>(u[1].account.userPrincipalName).toLowerCase() === userInfo.toLowerCase()) {
+        user = <Member>u[1];
+        break;
+      }
+      
+      if(u[1].FullNameKR === userInfo) {
+        user = <Member>u[1];
+        break;
+      }
+    }
+
+    if(user === null) {
+      text = text.substring(end+10);
+      continue;
+    }
+    
+    const mText = text.substring(start, end+10);
+    const rText = `<at> ${i} </at>`;
+    message = message.replace(mText, rText);
+
+    const mention: Mention = {
+        mentioned: user.account,
+        text: rText,
+        type: 'mention'
+    };
+    mentionArr.push(mention);
+    text = text.substring(end+10);
+  }
+
+  const messageActivity: Partial<Activity> = {
+    entities: mentionArr,
+    text: message,
+    type: ActivityTypes.Message
+  };
+
+  return messageActivity;
+}
+
+
+//삭제예정
 const SendMentionMessage = async (target: TeamsBotInstallation, username: string, messageText: string) => {
   if(!messageText || !username) {
     return {message: "Invalid request"};
