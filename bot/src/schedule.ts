63314220a36968436fbeaa674011d3d2f6de2549
KwangSeok.Moon <kwangseok.moon@cloudmt.co.kr>
2024-07-18 11:05:45 +0900

    업무 종료시간 전 근무지 알림 메시지 제거

--- bot/src/schedule.ts
+++ bot/src/schedule.ts
@@ -1,14 +1,14 @@
-import { setWorkplaceForm } from "./bot/setWorkplace";
+//import { setWorkplaceForm } from "./bot/setWorkplace";
 import { sendBirthdayCard } from "./bot/birthMessage";
 import { insertLog } from "./bot/common";
 
 const cron = require('node-cron');
 
 export const initCron = () => {  
-  cron.schedule('00 30 08 * * *', async () => {  
-    await insertLog('setWorkplaceForm send', `오늘 하루도 고생많으셨습니다.`);
-    await setWorkplaceForm(null, null, null, 'send');
-  });
+  // cron.schedule('00 30 08 * * *', async () => {  
+  //   await insertLog('setWorkplaceForm send', `오늘 하루도 고생많으셨습니다.`);
+  //   await setWorkplaceForm(null, null, null, 'send');
+  // });
   
   //생일자에게 카드 전송
   cron.schedule('00 30 01 * * *', async () => {  
